7a204600590e15ea533590245b1f8f6182b4cc55
Shammi Anand <shammianand101@gmail.com>
2020-10-01 15:51:52 +0530

    improved search

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,13 +1,13 @@
 """
-
 This code is the work of Shammi Anand
-
 """
 
+#----------------------------------------------------------------------
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+#----------------------------------------------------------------------
 
 def get_title_from_index(index):
 	return df[df.index == index]["title"].values[0]
@@ -15,42 +15,54 @@ def get_title_from_index(index):
 def get_index_from_title(title):
 	return df[df.title == title]["index"].values[0]
 
-def match(user_choice):
+def match_name(user_choice):
 	exists = False
 	for name in df['title']:
 		if name == movie_user_likes:
 			exists = True
 			break
 	return exists
+#----------------------------------------------------------------------
 
 df = pd.read_csv("movie_dataset.csv")
 movie_user_likes = input("Enter the name of movie you like : ")
-while(match(movie_user_likes) == False):
+
+#----------------------------------------------------------------------
+
+def find_similar_movies(words):
+	number = 1
+	size = len(words)
+	for name in df['title']:
+		total = 0
+		for word in words:
+			counter = 0
+			n = len(word)
+			for c in name:
+				if counter == n:
+					break
+				if c == ' ':
+					continue
+				if c == word[counter]:
+					counter += 1
+			if counter == n:
+				total += 1
+		if total == size:
+			print(f"{number}. {name}")
+			number += 1
+
+if match_name(movie_user_likes) == False :
 	print("This movie does not exists!")
 	words = list(movie_user_likes.split(" "))
 	words.sort(key = lambda x:len(x), reverse=True)
 	new_words = [word.capitalize() for word in words]
-	checker = ""
 	print("Did you mean : ")
-	first = True
-	for word in new_words:
-		if first :
-			first = False
-			checker += word
-		else :
-			checker += " "
-			checker += word
-		for name in df['title']:
-			if checker in name:
-				print(name)
-
+	find_similar_movies(new_words)
 	movie_user_likes = input("Enter the name of movie you like : ")
-
-
+#----------------------------------------------------------------------
 features = ['keywords','cast','genres','director']
-
 for feature in features:
 	df[feature] = df[feature].fillna('')
+
 
 def combine_features(row):
 	try:
@@ -70,9 +82,14 @@ cosine_sim = cosine_similarity(count_matrix)
 movie_index = get_index_from_title(movie_user_likes)
 similar_movies =  list(enumerate(cosine_sim[movie_index]))
 sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
-i=0
+
+#----------------------------------------------------------------------
+
+print("Here are the top 10 similar movies we recommend : ")
+i=1
 for element in sorted_similar_movies:
-		print(get_title_from_index(element[0]))
-		i=i+1
-		if i>50:
+		print(f"{i}. {get_title_from_index(element[0])}")
+		i+=1
+		if i>10:
 			break
+#----------------------------------------------------------------------
